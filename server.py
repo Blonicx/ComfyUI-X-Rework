ee0b8f7db2138867608f792bd5bbd8fcc5d58292
Blonicx <Belte_YT@outlook.com>
2025-01-21 21:42:26 +0100

    Implemented the Infos

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ mainpage_files = os.path.join(pathlib.Path(__file__).parent.resolve(), "src", "w
 
 ## Main Page ##
 @server.PromptServer.instance.routes.get("/xrework")
-async def get_gallery(request):
+async def get_index_page(request):
     response = web.FileResponse(os.path.join(mainpage_files, "index.html"))
     response.headers['Cache-Control'] = 'no-cache'
     response.headers["Pragma"] = "no-cache"
