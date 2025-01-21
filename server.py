ee7a794ec31e532c2ab49a4824861cf0ab035fc9
Blonicx <Belte_YT@outlook.com>
2025-01-21 13:47:11 +0100

    Remove Gallery

--- server.py
+++ server.py
@@ -6,21 +6,11 @@ web = server.web
 
 ## Files ##
 mainpage_files = os.path.join(pathlib.Path(__file__).parent.resolve(), "src", "web", "index_website")
-gallery_files = os.path.join(pathlib.Path(__file__).parent.resolve(), "src", "web", "gallery")
 
 ## Main Page ##
 @server.PromptServer.instance.routes.get("/xrework")
 async def get_gallery(request):
     response = web.FileResponse(os.path.join(mainpage_files, "index.html"))
-    response.headers['Cache-Control'] = 'no-cache'
-    response.headers["Pragma"] = "no-cache"
-    response.headers["Expires"] = "0"
-    return response
-
-## Setup Gallery ##
-@server.PromptServer.instance.routes.get("/gallery")
-async def get_gallery(request):
-    response = web.FileResponse(os.path.join(gallery_files, "gallery.html"))
     response.headers['Cache-Control'] = 'no-cache'
     response.headers["Pragma"] = "no-cache"
     response.headers["Expires"] = "0"
